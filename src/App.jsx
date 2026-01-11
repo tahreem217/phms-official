444d94da175bf67ce3b51e1b5bbb31a02822890d
tahreem217 <tahreemfatima21766@gmail.com>
2026-01-11 12:54:46 +0530

    Fix routing and configure Vite for GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
@@ -10,30 +10,31 @@ import About from "./components/About";
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Navbar />
+    <HashRouter>
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
 
-      <main className="flex-grow  pb-0 mb-0">
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Main />
-                <Notices />
-                <Thought />
-              </>
-            }
-          />
+        <main className="flex-grow pb-0 mb-0">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Main />
+                  <Notices />
+                  <Thought />
+                </>
+              }
+            />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/admission" element={<Admission />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </main>
 
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/admission" element={<Admission />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </main>
-
-      <Footer />
-    </div>
+        <Footer />
+      </div>
+    </HashRouter>
   );
 }
 
